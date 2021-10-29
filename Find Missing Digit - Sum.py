37d71cbd012d8db20ce9b8da038ca657c671b432
Logesh <logeshkrishna08@gmail.com>
2021-10-29 22:02:57 +0530

    29-10-2021

---
+++ Find Missing Digit - Sum.py
@@ -0,0 +1,57 @@
+# Find Missing Digit - Sum
+# The program must accept N integers and their sum S, but eactly one digit is missing in an integer. The missing digit in the integer is denoted by an asterisk. The program must find the missing digit and print the N integers without missing any digits.
+
+# Boundary Condition(s):
+# 1 <= N <= 100
+# 0 <= Each integer value <= 10^6
+
+# Input Format:
+# The first line contains N.
+# The second line contains N integers separated by a space.
+# The third line contains S.
+
+# Output Format:
+# The first line contains N integers separated by a space.
+
+# Example Input/Output 1:
+# Input:
+# 3
+# 468 120 9*5
+# 1573
+
+# Output:
+# 468 120 985
+
+# Explanation:
+# Here N = 3 and a digit is missing in the 3rd integer.
+# If the missing digit is 8, then 468 + 120 + 985 = 1573.
+
+# Example Input/Output 2:
+# Input:
+# 4
+# 15 *8652 987 52364
+# 152018
+
+# Output:
+# 15 98652 987 52364
+
+
+
+
+
+
+m=int(input()) 
+l=input().split() 
+n=int(input()) 
+s=0 
+for i in l:  
+    if i.count("*")==0: 
+        s+=int(i) 
+    else: 
+        d=i 
+n-=s
+for i in l: 
+    if i.count("*")==0:
+        print(i,end=" ")
+    else: 
+        print(str(n),end=" ")
