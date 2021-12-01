f641e6ceaa0f0d2edb1d82f6e69b4857b19aef96
Logesh <logeshkrishna08@gmail.com>
2021-12-01 22:01:22 +0530

    01-12-2021

---
+++ function solveExpression.c
@@ -0,0 +1,49 @@
+// function solveExpression
+// The function/method solveExpression accepts an argument expression representing an expression as given below.
+
+// val1+val2=val3 or val1*val2=val3
+
+// In the above expression, one of the three values will be replaced with X.
+
+// The function/method solveExpression solve the given expression and find the value of X. Then the function must return the integer value X.
+
+// Your task is to implement the function solveExpression so that the program runs successfully.
+
+// IMPORTANT: Do not implement the main() function as it is already defined.
+
+// Example Input/Output 1:
+// Input:
+// 5+X=7
+
+// Output:
+// 2
+
+// Explanation:
+// If X = 2, then the expression becomes 5+2=7.
+
+// Example Input/Output 2:
+// Input:
+// 4*5=X
+
+// Output:
+// 20
+
+// Example Input/Output 3:
+// Input:
+// X+2=100
+
+// Output:
+// 98
+
+// Example Input/Output 4:
+// Input:
+// X*2=100
+
+// Output:
+// 50
+
+
+
+
+
+
