427297d925ded64ea1f4ec720c3d2d12f91453a8
Logesh <logeshkrishna08@gmail.com>
2021-12-05 22:02:07 +0530

    05-12-2021

---
+++ function splitAndConcat.c
@@ -0,0 +1,43 @@
+// function splitAndConcat
+// The function/method splitAndConcat accepts an argument str. The string str contains multiple words of even length.
+
+// The function/method splitAndConcat must split each word into two equal halves. Then the program must concatenate the second half of each word and the first half of its next word. Finally, the function must return the resulting string as a new string.
+
+// Your task is to implement the function splitAndConcat so that the program runs successfully.
+
+// IMPORTANT: Do not write the main() function as it is already defined.
+
+// Example Input/Output 1:
+// Input:
+// four lion keyboard on strawberry
+
+// Output:
+// fo urli onkeyb oardo nstraw berry
+
+// Explanation:
+// four -> fo and ur
+// lion -> li and on
+// keyboard -> keyb and oard
+// on -> o and n
+// strawberry -> straw and berry
+// After concatenating the second half of each word and the first half of its next word, the words become
+// fo urli onkeyb oardo nstraw berry
+
+// Example Input/Output 2:
+// Input:
+// abcdefghij ABCDEF abcd pqrstu
+
+// Output:
+// abcde fghijABC DEFab cdpqr stu
+
+
+
+
+
+
+
+
+
+
+
+
