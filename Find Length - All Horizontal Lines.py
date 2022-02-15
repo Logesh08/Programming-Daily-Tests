9d788d3993512cafafbcbc1bae7e04da9f8fd172
Logesh <logeshkrishna08@gmail.com>
2022-02-15 22:30:00 +0530

    15-02-2022

---
+++ Find Length - All Horizontal Lines.py
@@ -0,0 +1,80 @@
+# Find Length - All Horizontal Lines
+# The program must accept the coordinates (x, y) of N points as the input. The program must print the lengths of all possible horizontal lines that can be formed using the given N points as the output. The lengths of the horizontal lines must be printed in sorted order. If it is not possible to form horizontal lines, then the program must print -1 as the output.
+
+# Note: All N points are always distinct.
+
+# Boundary Condition(s):
+# 2 <= N <= 500
+# -10 <= x, y <= 10
+
+# Input Format:
+# The first line contains N.
+# The next N lines, each contains two integers separated by a space representing the coordinates(x, y) of a point.
+
+# Output Format:
+# The first line contains the integer values separated by a space representing the lengths of all possible horizontal lines that can be formed using the given N points.
+
+# Example Input/Output 1:
+# Input:
+# 9
+# 1 1
+# -2 -1
+# -2 3
+# -1 1
+# 2 -1
+# 0 3
+# 2 2
+# 4 -1
+# 2 1
+
+# Output:
+# 1 2 2 2 3 4 6
+
+# Explanation:
+# The start and end coordinates of the 7 possible horizontal lines are given below.
+# Length 1: (1, 1) to (2, 1)
+# Length 2: (-2, 3) to (0, 3)
+# Length 2: (-1, 1) to (1, 1)
+# Length 2: (2, -1) to (4, -1)
+# Length 3: (-1, 1) to (2, 1)
+# Length 4: (-2, -1) to (2, -1)
+# Length 6: (-2, -1) to (4, -1)
+
+# Example Input/Output 2:
+# Input:
+# 5
+# -2 2
+# 0 1
+# 1 3
+# -1 -1
+# -1 0
+
+# Output:
+# -1
+
+
+
+
+
+
+
+
+
+
+
+
+n = int(input())
+arr = [input() for _ in range(n)]
+dis = []
+for j in range(len(arr)):
+    
+    x,y = map(int,arr[j].split())
+    for i in arr[j:]:
+        a,b= map(int,i.split())
+        if i!=arr[j]:
+            if y == b:
+                dis.append(abs(a-x))
+if dis:
+    print(*sorted(dis))
+else:
+    print(-1)
