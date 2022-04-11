5659c4e306b5b311286d1131ae3a0a879a916495
Logesh <logeshkrishna08@gmail.com>
2022-04-11 22:12:18 +0530

    11-04-2022

---
+++ Group Words - Combinations.py
@@ -0,0 +1,64 @@
+# Group Words - Combinations
+# The program must accept a string S containing multiple words as the input. The program must group the words where each word is a combination of the other words. Then the program must print the groups of words in separate lines in the order of their occurrence.
+
+# Boundary Condition(s):
+# 3 <= Length of S <= 1000
+
+# Input Format:
+# The first line contains S.
+
+# Output Format:
+# The lines contain the groups of words separated by a space based on the given conditions.
+
+# Example Input/Output 1:
+# Input:
+# neon nose ones race none noes care
+
+# Output:
+# neon none
+# nose ones noes
+# race care
+
+# Explanation:
+# Here S = "neon nose ones race none noes care".
+# 1st group: neon none
+# 2nd group: nose ones noes
+# 3rd group: race care
+# In each group, each word is a combination of the other words in the group.
+# Hence the output is
+# neon none
+# nose ones noes
+# race care
+
+# Example Input/Output 2:
+# Input:
+# seven oil evens lio smart loi skill
+
+# Output:
+# seven evens
+# oil lio loi
+# smart
+# skill
+
+
+
+
+
+
+
+
+
+
+
+s = input().split()
+ans = []
+for i in s:
+    t = []
+    for j in s:
+        if sorted(list(i))==sorted(list(j)):
+            t.append(j)
+    if t:
+        if t not in ans:
+            ans.append(t)
+for a in ans:
+    print(*a)
