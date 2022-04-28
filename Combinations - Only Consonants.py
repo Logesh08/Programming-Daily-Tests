b7d005c4247176e928ac44ca1edf0a9c7b9fba13
Logesh <logeshkrishna08@gmail.com>
2022-04-28 15:38:44 +0530

    28-04-2022

--- Combinations - Only Consonants.py
+++ Combinations - Only Consonants.py
@@ -62,22 +62,37 @@
 
 
 
-from itertools import combinations
-s = input().strip()
-v='aeiouAEIOU'
-l  = len(s)
-printed = False
-arr = []
-for i in range(l):
-    for j in range(i+1,l):
-        if s[i] not in v and s[j] not in v:
-            if s[i] not in arr:
-                arr.append(s[i])
-            if s[j] not in arr:
-                arr.append(s[j])
-for i in range(2,len(arr)+1):
-    for i in (list(combinations(arr,i))):
-        print(''.join(i))
-        printed = True
-if not printed:
+# from itertools import combinations
+# s = input().strip()
+# v='aeiouAEIOU'
+# l  = len(s)
+# printed = False
+# arr = []
+# for i in range(l):
+#     for j in range(i+1,l):
+#         if s[i] not in v and s[j] not in v:
+#             if s[i] not in arr:
+#                 arr.append(s[i])
+#             if s[j] not in arr:
+#                 arr.append(s[j])
+# for i in range(2,len(arr)+1):
+#     for i in (list(combinations(arr,i))):
+#         print(''.join(i))
+#         printed = True
+# if not printed:
+#     print(-1)
+
+l=[x for x in input().strip() if x not in "aeiouAEIOU"]
+if len(l)<2:
     print(-1)
+    quit()
+else:
+    for i in range(3,2**(len(l))):
+        t=i
+        if((i & (~(i-1)))==i):
+            continue
+        for j in l:
+            if t%2==1:
+                print(j,end="")
+            t//=2
+        print()
