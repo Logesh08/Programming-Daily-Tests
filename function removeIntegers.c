43ffab6f4256bde88aea3e76607011cff84652b0
Logesh <logeshkrishna08@gmail.com>
2022-05-05 22:15:23 +0530

    05-05-2022

---
+++ function removeIntegers.c
@@ -0,0 +1,48 @@
+// function removeIntegers
+// The function/method removeIntegers accepts an argument fileName representing the name of a text file. The given text file contains integer values and string values.
+
+// The function/method removeIntegers must remove the integer values in the given file.
+
+// Note:
+// - The number of characters in each line of the given file does not exceed thousand characters.
+// - There will be at least one string value in each line.
+
+// Your task is to implement the function removeIntegers so that the program runs successfully.
+
+// IMPORTANT: Do not write the main() function as it is already defined.
+
+// Example Input/Output 1:
+// Input:
+// numbers.txt
+
+// Output:
+// numbers.txt:
+// One Five
+// Seven Nine Ten
+// Eleven
+// Fourteen Fifteen
+
+// Explanation:
+// Here numbers.txt is present in the same folder where the program executes and the file contains the following values.
+// One 2 3 4 Five
+// 6 Seven 8 Nine Ten
+// Eleven 12 13
+// Fourteen Fifteen
+
+// Example Input/Output 2:
+// Input:
+// greetings.txt
+
+// Output:
+// greetings.txt:
+// Hi Hello Welcome
+// Good Morning
+// Good Evening
+// Good Night
+
+// Explanation:
+// Here greetings.txt is present in the same folder where the program executes and the file contains the following values.
+// Hi 15 Hello 123 Welcome
+// 45 Good Morning
+// Good Evening
+// Good Night 56 34 12
