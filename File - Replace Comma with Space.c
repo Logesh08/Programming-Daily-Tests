f0ea0ecb43ca961d0ca066c8c19f6b5d238ce2e1
Logesh <logeshkrishna08@gmail.com>
2022-05-18 22:03:30 +0530

    18-05-2022

---
+++ File - Replace Comma with Space.c
@@ -0,0 +1,44 @@
+// File - Replace Comma with Space
+// The function/method replaceCommaWithSpace accepts an argument fileName representing the name of a text file. The given text file is always present in the same folder where the program executes and it contains comma-separated integer values in each line.
+
+// The function/method replaceCommaWithSpace must modify the content of the given file by replacing each comma with a space.
+
+// Your task is to implement the function replaceCommaWithSpace so that the program runs successfully.
+
+// IMPORTANT: Do not write the main() function as it is already defined.
+
+// Example Input/Output 1:
+// Input:
+// numbers.txt
+
+// Output:
+// numbers.txt:
+// 15 20 90 5465 345
+// 15500 15
+// 1 2 3 4 5 6 7 8 9 10 100 1000
+
+// Explanation:
+// Here numbers.txt is present in the same folder where the program executes and the file contains the following values.
+// 15,20,90,5465,345
+// 15500,15
+// 1,2,3,4,5,6,7,8,9,10,100,1000
+
+// Example Input/Output 2:
+// Input:
+// integers.txt
+
+// Output:
+// integers.txt:
+// 36 73 12 58 19 58 29
+// 66 67 28
+// 75 77 67 6 94
+// 49
+// 49 40 12 39 28 7
+
+// Explanation:
+// Here integers.txt is present in the same folder where the program executes and the file contains the following values.
+// 36,73,12,58,19,58,29
+// 66,67,28
+// 75,77,67,6,94
+// 49
+// 49,40,12,39,28,7
