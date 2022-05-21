c184ed594a094b26e978a08d3e42e589bffdd037
Logesh <logeshkrishna08@gmail.com>
2022-05-21 22:09:02 +0530

    21-05-2022

---
+++ Alphabet Difference Two Strings.py
@@ -0,0 +1,28 @@
+# Alphabet Difference Two Strings
+# Given two strings S1 and S2, the program must print Yes if the alphabets in the same position in two strings differ at the most by 1 else print No.
+
+# Boundary Condition(s):
+# 1 <= Length of S1 and S2 <= 1000
+
+# Input Format:
+# The first line contains S1.
+# The second line contains S2.
+
+# Output Format:
+# The first line contains Yes or No.
+
+# Example Input/Output 1:
+# Input:
+# hello
+# gemkp
+
+# Output:
+# Yes
+
+# Example Input/Output 2:
+# Input:
+# best
+# agts
+
+# Output:
+# No
