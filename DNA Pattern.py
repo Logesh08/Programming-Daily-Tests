ceea6e5bd96625c5a32a0ddba2fb5f6d6e472403
Logesh <logeshkrishna08@gmail.com>
2022-08-14 22:27:07 +0530

    14-08-2022

---
+++ DNA Pattern.py
@@ -0,0 +1,55 @@
+# DNA Pattern
+
+# The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+# Note: N must be always an even integer.
+
+# Boundary Condition:
+# 4 <= N <= 40
+
+# Input Format:
+# The first line contains the value of N.
+
+# Output Format:
+# The list of lines contain the desired pattern as shown in the Example Input/Output section.
+
+# Example Input/Output 1:
+# Input:
+# 4
+
+# Output:
+# -**
+# *--*
+# -**
+# *--*
+# -**
+
+# Example Input/Output 2:
+# Input:
+# 8
+
+# Output:
+# ---**
+# --*--*
+# -*----*
+# *------*
+# -*----*
+# --*--*
+# ---**
+# --*--*
+# -*----*
+# *------*
+# -*----*
+# --*--*
+# ---**
+# --*--*
+# -*----*
+# *------*
+# -*----*
+# --*--*
+# ---**
+# --*--*
+# -*----*
+# *------*
+# -*----*
+# --*--*
+# ---**
